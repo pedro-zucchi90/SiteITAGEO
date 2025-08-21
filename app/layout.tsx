ccf38f950ae1f46fab85655ef21bb5b82d32845b
Pedro Zucchi <predozucchi@gmail.com>
2025-08-21 10:43:39 -0300

    adicionando speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout({
         <main className="pt-16 md:pt-20">{children}</main>
         <Footer />
         <ScrollToTop />
+        <SpeedInsights/>
       </body>
     </html>
   )
