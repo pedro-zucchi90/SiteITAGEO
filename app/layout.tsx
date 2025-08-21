e4e8b6e3248bf9f596db4672676326ba0e87225b
Pedro Zucchi <predozucchi@gmail.com>
2025-08-20 21:57:25 -0300

    adicionando SpeedInsights do Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import { Navbar } from "@/components/navbar"
 import { Footer } from "@/components/footer"
 import { ScrollToTop } from "@/components/scroll-to-top"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
